881df4ef20401a2123ca2e0498ab0dbe0709deab
unknown <SENA@MEDAPRCCSFSP409.FormacionSena.RED>
2021-10-20 16:57:06 -0500

    a

---
+++ inc/inicio.php
@@ -0,0 +1,22 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./bootstrap-CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="css/Index.css">
+</head>
+
+<ul class="nav justify-content-end">
+    <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="index.php">Nosotros</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Regsitro</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="Login.php">Iniciar sesion</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contactos</a>
+    </li>
+</ul>
