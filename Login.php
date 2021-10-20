b65bb87730065cacd16f97188a8bee5cc7efaa3e
unknown <SENA@MEDAPRCCSFSP409.FormacionSena.RED>
2021-10-20 16:18:00 -0500

    un commit mas

--- Login.php
+++ Login.php
@@ -10,7 +10,18 @@
 </head>
 
 <body>
-    
+    <form class="form">
+        <h2>Login</h2>
+        <p type="Cedula:"><input placeholder="Ingrese su Cedula"></input></p>
+        <p type="Nombre:"><input placeholder="Ingrese su Nombre"></input></p>
+        <p type="Rol:"><input placeholder="Ingresesu Rol"></input></p>
+        <button>Iniciar Sesion</button>
+      </form>
 </body>
-
+<footer>
+    <div>
+        <span class="fa fa-phone"></span>001 1023 567
+        <span class="fa fa-envelope-o"></span> MH-HISTORY@company.com
+      </div>
+</footer>
 </html>
