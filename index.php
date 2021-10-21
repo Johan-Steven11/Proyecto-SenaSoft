0f95177cda0b06ab69a3af92851270477412239a
unknown <SENA@MEDAPRCCSFSP409.FormacionSena.RED>
2021-10-21 11:36:45 -0500

    index2

--- index.php
+++ index.php
@@ -1,40 +1,204 @@
-<?php
-include './inc/inicio.php';
-?>
+
 
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>MH-HISTORY</title>
-  
-  <link rel="stylesheet" href="./bootstrap-CSS/bootstrap.min.css">
-  <link rel="stylesheet" href="css/Index.css">
-</head>
-
-<body>
-
-
-
-  <main>
-    <section>
-      <div>
-        <img src="https://www.pexels.com/es-es/foto/doctor-sentado-frente-a-su-escritorio-2182979/" alt="">
-        
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>felicity</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css-index/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css-index/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css-index/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="img/ImgI/fevicon.png" type="img/ImgI/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css-index/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><img src="img/ImgI/loading.gif" alt="#" /></div>
       </div>
-    </section>
-  </main>
-
- 
-
-
-
-
-  <script src="./bootstrap-js/bootstrap.bundle.min.js"></script>
-  </div>
-
-
+      <!-- end loader -->
+      <!-- header -->
+      <header>
+         <!-- header inner -->
+         <div class="header">
+            <div class="container">
+               <div class="row">
+                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                     <div class="full">
+                        <div class="center-desk">
+                           <div class="logo">
+                              <a href="index.php"><img src="img/ImgI/logo.png" alt="#" /></a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                     <nav class="navigation navbar navbar-expand-md navbar-dark ">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarsExample04">
+                           <ul class="navbar-nav mr-auto">
+                              <li class="nav-item">
+                                 <a class="nav-link" href="">Nosotros</a>
+                              </li>
+                              <li class="nav-item">
+                                 <a class="nav-link" href="#contact">Contacto</a>
+                              </li>
+                           </ul>
+                           <div class="sign_btn"><a href="Login.php">Inicio secion</a></div>
+                        </div>
+                     </nav>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </header>
+      <!-- end header inner -->
+      <!-- end header -->
+      <!-- banner -->
+      <section class="banner_main" id="x">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="text-bg">
+                     <div class="padding_lert">
+                        <h1>Bienvenido a M-History</h1>
+                        <span>Ofreciendo nuestros servicios desde el  2019</span>
+                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
+                        <a href="#">Read More</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- end banner -->
+      <!-- form_lebal -->
+      <section>
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <form class="form_book">
+                     <div class="row">
+                        <div class="col-md-3">
+                           <label class="date">ARRIVAL DATE</label>
+                           <input class="book_n"  type="date" >
+                        </div>
+                        <div class="col-md-3">
+                           <label class="date">DEPARTURE DATE</label>
+                           <input class="book_n"  type="date" >
+                        </div>
+                        <div class="col-md-3">
+                           <label class="date">PERSON</label>
+                           <input class="book_n" placeholder="2" type="type" name="2">
+                        </div>
+                        <div class="col-md-3">
+                           <button class="book_btn">Book Now</button>
+                        </div>
+                     </div>
+                  </form>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- end form_lebal -->
+      <!-- choose  section -->
+      <div class="choose">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-6">
+                  <div class="choose_box">
+                     <div class="titlepage">
+                        <h2><span class="text_norlam">Choose The Perfect</span> <br>Accommodation</h2>
+                     </div>
+                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit </p>
+                     <a class="read_more" href="#">See More</a>
+                  </div>
+               </div>
+               <div class="col-md-6">
+                  <div class="row">
+                     <div class="col-md-12">
+                        <div class="img_box">
+                           <figure><img src="img/ImgI/img1.jpg" alt="#"/></figure>
+                        </div>
+                     </div>
+                     <div class="col-md-6">
+                        <div class="img_box">
+                           <figure><img src="img/ImgI/img2.jpg" alt="#"/></figure>
+                        </div>
+                     </div>
+                     <div class="col-md-6">
+                        <div class="img_box">
+                           <figure><img src="img/ImgI/img3.jpg" alt="#"/></figure>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end choose  section -->
+      <!-- our  section -->
+      <div class="our">
+         <div class="container">
+            <div class="row d_flex">
+               <div class="col-md-6">
+                  <div class="img_box">
+                     <figure><img src="img/ImgI/img4.jpg" alt="#"/></figure>
+                  </div>
+               </div>
+               <div class="col-md-6">
+                  <div class="our_box">
+                     <div class="titlepage">
+                        <h2><span class="text_norlam">Our Best </span> <br>Breakfast</h2>
+                     </div>
+                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit </p>
+                     <a class="read_more" href="#">Read More</a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end our  section -->
+      <!-- about -->
+    
+      <!-- end about -->
+      <!-- testimonial -->
+     
+            
+    
+      <!-- Javascript files-->
+      <script src="java_index/jquery.min.js"></script>
+      <script src="java_index/popper.min.js"></script>
+      <script src="java_index/bootstrap.bundle.min.js"></script>
+      <script src="java_index/jquery-3.0.0.min.js"></script>
+      <script src="java_index/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="java_index/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="java_index/custom.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+   </body>
 </html>
+
